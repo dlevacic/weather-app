76cec5609e21074aafc226737b815cb0fe23d935
David Levacic <dlevacic@tria.hr>
2020-09-21 11:56:32 +0200

    changed h1

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export class Header extends Component {
     render() {
         return (
             <div>
-                <h1>danchy App!</h1>
+                <h1>IK Weather App!</h1>
             </div>
         )
     }
